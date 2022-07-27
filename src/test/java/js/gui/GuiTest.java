279a2e2ec0c972783806dfdf5ac1430e07986eb7
Jeff Sember <jpsember@gmail.com>
2022-07-27 16:30:20 -0700

    Initial commit

---
+++ src/test/java/js/gui/GuiTest.java
@@ -0,0 +1,15 @@
+package js.gui;
+
+import org.junit.Test;
+
+import static js.base.Tools.*;
+import js.testutil.MyTestCase;
+
+public class GuiTest extends MyTestCase {
+
+  @Test
+  public void example() {
+    todo("Add some unit tests.");
+  }
+
+}
