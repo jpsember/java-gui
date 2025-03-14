3d462b86612a31cbb8d4f43deb52f750f305d7e6
jeff <jeff@urbanlogiq.com>
2025-03-13 18:11:21 -0700

    Minor changes

--- src/main/java/js/guiapp/GUIApp.java
+++ src/main/java/js/guiapp/GUIApp.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- * 
+ *
  * Copyright (c) 2021 Jeff Sember
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- * 
  **/
 package js.guiapp;
 
@@ -84,7 +83,7 @@ public abstract class GUIApp extends App {
     return guiAppConfig().version();
   }
 
-  private GuiAppConfig.Builder mConfig = GuiAppConfig.DEFAULT_INSTANCE.toBuilder();
+  private GuiAppConfig.Builder mConfig = GuiAppConfig.DEFAULT_INSTANCE.toBuilder().devMode(false);
 
   // ------------------------------------------------------------------
   // Command line arguments
@@ -245,7 +244,7 @@ public abstract class GUIApp extends App {
 
   /**
    * Trigger a repaint of various app components
-   * 
+   *
    * @param repaintFlags
    *          a combination of REPAINT_xxx
    */
--- src/main/java/js/guiapp/MenuBarWrapper.java
+++ src/main/java/js/guiapp/MenuBarWrapper.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- * 
+ *
  * Copyright (c) 2021 Jeff Sember
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- * 
  **/
 package js.guiapp;
 
@@ -91,13 +90,7 @@ public final class MenuBarWrapper {
       if (hotKey != null) {
         mgr.assignHotKeyToOperation(hotKeyId, hotKey);
         KeyStroke s = KeyboardShortcutManager.compileKeystroke(hotKey);
-//        if (ISSUE_2) {
-//          if (hotKeyId.equals("script_step_fwd")) {
-//            pr("set accelerator for:", hotKeyId, TAB(40), "to:", s);
-//            menuItem.setAccelerator(s);
-//          }
-//        } else
-          menuItem.setAccelerator(s);
+        menuItem.setAccelerator(s);
       } else {
         alert("no hot key found with id:", hotKeyId);
       }
